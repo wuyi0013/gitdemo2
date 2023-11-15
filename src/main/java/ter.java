27bc338c5ba39f033b66e3bdd49ba134dd87b526
wuyi0013 <2676204234@qq.com>
2023-11-15 17:14:02 +0800

    java

---
+++ src/main/java/ter.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: ter
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author: 杨怀志
+ * @Create: 2023/11/15 - 17:13
+ * @Version: v1.0
+ */
+public class ter {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
